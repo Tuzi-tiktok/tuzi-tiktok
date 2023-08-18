8d05d5702389b2798880e3b378d6d983d0d551e5
Aliothmoon <aliothmoon@outlook.com>
2023-08-18 10:52:57 +0800

    Feat(gateway): Add Access trace id T:

--- internal/gateway/biz/err/handler.go
+++ internal/gateway/biz/err/handler.go
@@ -3,6 +3,7 @@ package err
 import (
 	"context"
 	"errors"
+	"github.com/bytedance/gopkg/lang/fastrand"
 	"github.com/cloudwego/hertz/pkg/app"
 	"tuzi-tiktok/gateway/biz/err/global"
 	"tuzi-tiktok/logger"
@@ -12,11 +13,13 @@ const (
 	code = "status_code"
 	msg  = "status_msg"
 )
+const maxTraceId = 1000
 
 // ErrorHandlerMiddleware Handle Global Error
 func ErrorHandlerMiddleware() app.HandlerFunc {
 	return func(c context.Context, ctx *app.RequestContext) {
-		logger.Debugf("Access %v", ctx.FullPath())
+		tid := fastrand.Int31n(maxTraceId)
+		logger.Debugf("T: %d Access %v", tid, ctx.FullPath())
 		ctx.Next(c)
 		err := ctx.Errors.Last()
 		if err != nil && err.Err != nil {
@@ -28,10 +31,10 @@ func ErrorHandlerMiddleware() app.HandlerFunc {
 			var ret = make(map[string]any, 2)
 			switch exception.InternalStatusCode {
 			case global.InvalidTokenOrUnauthorizedCode, global.TokenNotFoundCode:
-				logger.Warnf("Required Token is Not Found or InvalidToken Handler :%v; msg :%v;", exception.HandlerName, exception.StatusMessage)
+				logger.Warnf("T: %d Required Token is Not Found or InvalidToken Handler :%v; msg :%v;", tid, exception.HandlerName, exception.StatusMessage)
 				ret[msg] = "Authentication failure Check Your Token"
 			default:
-				logger.Warnf("Service Face %v Catch Internal Error is %v", exception.HandlerName, exception.StatusMessage)
+				logger.Warnf("T: %d Service Face %v Catch Internal Error is %v", tid, exception.HandlerName, exception.StatusMessage)
 				ret[msg] = "Service Unavailable Check Server Log"
 			}
 			ret[code] = exception.InternalStatusCode
