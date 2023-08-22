bcdc7cf367f827b848fa15f2ab5e2b059f5b3f2b
Aliothmoon <aliothmoon@outlook.com>
2023-08-23 00:01:31 +0800

    Feat(config):  Add few remote config unit test

--- shared/config/test/config_test.go
+++ shared/config/test/config_test.go
@@ -1,12 +1,49 @@
 package test
 
 import (
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/vo"
 	"log"
+	"os"
 	"testing"
-	cfg "tuzi-tiktok/config"
+	. "tuzi-tiktok/config"
 )
 
 func TestConfig(t *testing.T) {
-	log.Printf(cfg.LoggerConfig.Encoding)
-	log.Printf("%#v", cfg.LoggerConfig)
+	log.Printf(LoggerConfig.Encoding)
+	log.Printf("%#v", LoggerConfig)
+}
+
+func TestRemoteConfig(t *testing.T) {
+	clientConfig := *constant.NewClientConfig(
+		constant.WithNamespaceId(Registration.NamespaceId),
+		constant.WithTimeoutMs(5000),
+		constant.WithLogLevel("debug"),
+		constant.WithLogDir(os.TempDir()),
+		constant.WithCacheDir(os.TempDir()),
+		constant.WithNotLoadCacheAtStart(true),
+		constant.WithLogStdout(false),
+	)
+	serverConfigs := []constant.ServerConfig{
+		{
+			IpAddr: Registration.Host,
+			Port:   Registration.Port,
+		},
+	}
+	client, err := clients.NewConfigClient(vo.NacosClientParam{
+		ClientConfig:  &clientConfig,
+		ServerConfigs: serverConfigs,
+	})
+	if err != nil {
+		panic(err)
+	}
+	c, err := client.GetConfig(vo.ConfigParam{
+		Group:  Registration.Group,
+		DataId: Registration.DataId,
+	})
+	if err != nil {
+		t.Error(c)
+	}
+	log.Println(c)
 }
--- shared/redis/client_test.go
+++ shared/redis/client_test.go
@@ -2,6 +2,7 @@ package rds
 
 import (
 	"context"
+	"github.com/redis/go-redis/v9"
 	"log"
 	"testing"
 )
@@ -12,4 +13,20 @@ func TestClient(t *testing.T) {
 	ping := IRC.Ping(context.TODO())
 	log.Println(ping.Result())
 
+}
+
+func TestCfgKeys(t *testing.T) {
+	result, err := IRC.Keys(context.TODO(), "*").Result()
+	if err != nil {
+		t.Error(err)
+	}
+	log.Println(result)
+}
+func TestNilHost(t *testing.T) {
+	client := redis.NewClient(&redis.Options{})
+	result, err := client.Keys(context.TODO(), "*").Result()
+	if err != nil {
+		t.Error(err)
+	}
+	log.Println(result)
 }
